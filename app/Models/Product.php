9dafa825eb9eb079a458f5dd12f606ba714e08f3
Rizi5846 <rizkiardiansyah584@gmail.com>
2024-05-09 17:29:52 +0700

    Tugas Laravel Storage and Validate

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,10 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'gambar',
+        'nama',
+        'berat',
+        'harga',
+        'kondisi',
+        'stok',
+        'deskripsi'
+    ];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    
 }
--- database/migrations/2024_05_08_090731_create_user_profile_table.php
+++ database/migrations/2024_05_08_090731_create_user_profile_table.php
@@ -9,21 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('tokos', function (Blueprint $table) {
+        Schema::create('user_profile', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('nama');
-            $table->string('alamat');
-            $table->string('telepon');
-            $table->string('product_terbaik');
-            $table->text('deskripsi')->nullable();
+            $table->string('merchant_name');
+            $table->enum('rate', [1,2,3,4,5]);
+            $table->string('best_seller_product');
+            $table->text('description');
             $table->timestamps();
-            
+
             $table->foreign('user_id')->references('id')->on('users');
         });
-        
     }
 
     /**
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tokos');
+        Schema::dropIfExists('user_profile');
     }
 };
